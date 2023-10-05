4aef8835a3d06bd7c685cd64a68ea6ed672980d9
Dimitris Gouveris <dgg85@yahoo.gr>
2023-10-05 17:01:03 +0300

    adds axios, tailwind forms, page navigation, routing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8">
+    <input type="text" class="rounded border-2 border-gray-200 w-full" 
+    placeholder="Search for Meals">
+
+    <div class="flex justify-center gap-2 mt-2">
+      <RouterLink :to="{name: 'byLetter', params:{letter}}" v-for="letter of letters.split('')" :key="letter">
+        {{ letter }}
+      </RouterLink>
+    </div>
+
+  </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
+import axiosClient from '../axiosClient.js';
 
 
-const meals = computed( ()=> store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const ingredients = ref([]);
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list');
+  console.log(response.data);
+  ingredients.value = response.data;
+});
 
 </script>
