97f3fffeb3ac75dd5c081d3a50d77ba994710e9e
Dimitris Gouveris <dgg85@yahoo.gr>
2023-10-10 13:17:35 +0300

    adds search pages + layouts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,5 @@
 <template>
-  <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow h-16 flex justify-between items-stretch">
-      <RouterLink :to="{name: 'home'}" class="inline-flex items-center px-5">
-        Home
-      </RouterLink>
-      <div class="flex items-center gap-1">
-        <RouterLink to='/' class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">Search Meals</RouterLink>
-        <RouterLink to='/' class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">Meals By Letter</RouterLink>
-        <RouterLink to='/' class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">Meals By Ingredients</RouterLink>
-      </div>
-    </header>
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <RouterView />
 </template>
 
 <script setup>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+      <RouterLink :to="{name: 'home'}" class="inline-flex items-center px-5">
+        Home
+      </RouterLink>
+      <div class="flex items-center gap-1">
+        <RouterLink :to="{name:'byName'}" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
+          Search Meals
+        </RouterLink>
+        <RouterLink :to="{name:'byLetter'}" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
+          Meals By Letter
+        </RouterLink>
+        <RouterLink :to="{name:'byIngredient'}" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
+          Meals By Ingredient
+        </RouterLink>
+      </div>
+    </header>
+</template>
