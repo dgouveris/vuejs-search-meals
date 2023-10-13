cd03158971eb1e1d012ed06bfb9a52b5d3a1d631
Dimitris Gouveris <dgg85@yahoo.gr>
2023-10-13 17:41:27 +0300

    adds mealDetails implementation

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -5,8 +5,8 @@
   </div>
 
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <div v-for="meal of meals" :key="meal.idMeals" class="bg-white shadow rounded-xl">
-      <router-link to="/">
+    <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
+      <router-link :to="{name: 'mealDetails', params:{id: meal.idMeal}}">
         <img :src="meal.strMealThumb" :alt="strMeal" class="rounded-t-xl w-full h-48 object-cover">
       </router-link>
       <div class="p-3">
